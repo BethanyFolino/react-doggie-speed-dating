cab4e58c69b6a79ed45cfd4ed1aa4e882399b210
BETHANY FOLINO <bethany.folino@gmail.com>
2021-02-16 21:32:01 -0800

    Put in useState and useEffect with fetch

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-const {useState, useEffect} = React;
+const { useState, useEffect } = React;
 
 function App() {
   // Your Code Here
+  const [dog, setDog] = useState({});
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((object) => object.json())
+      .then((hydrated) => setDog(hydrated));
+  });
 
   return (
     <div className="app">
       <h1>Doggie Speed Dating</h1>
-      <h3>Press the arrow keys on your keyboard. Left to skip, Right to Adopt.</h3>
-      <section>
-        Your HTML Here!
-      </section>
+      <h3>
+        Press the arrow keys on your keyboard. Left to skip, Right to Adopt.
+      </h3>
+      <section>{dog}</section>
     </div>
   );
 }
 
 //Render the application
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
