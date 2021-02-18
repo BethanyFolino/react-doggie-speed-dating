54656ff143070a3b9abf29684e3a4dc4d30968ec
BETHANY FOLINO <bethany.folino@gmail.com>
2021-02-17 20:40:02 -0800

    The app is functional - event listeners and timer work

--- App.js
+++ App.js
@@ -4,36 +4,50 @@ function App() {
   // Your Code Here
   const [dog, setDog] = useState([]);
   const [adopted, adoptDog] = useState([]); //an array for adopted dogs to go into
+  const [timeRemaining, setTimeRemaining] = useState(5);
 
   //componentDidMount
   useEffect(() => {
+    getNewDog();
+  }, []);
+
+  const getNewDog = (e) => {
     fetch("https://dog.ceo/api/breeds/image/random")
       .then((res) => res.json())
       .then((data) => setDog(data.message));
-  }, []);
+  };
 
   const arrowKeys = (e) => {
-    if (e.code === "ArrowLeft") {
+    if (e.key === "ArrowLeft") {
       //move on to next dog
-      setDog(); //makes picture go away, but then it is stuck until refreshed
-    } else if (e.code === "ArrowRight") {
+
+      getNewDog();
+    } else if (e.key === "ArrowRight") {
       //put current dog picture in adopted array, then get new dog picture
-      adoptDog(adopted);
-      setDog();
+
+      adoptDog([...adopted, dog]);
     }
   };
 
   //componentDidUpdate
   useEffect(() => {
     window.addEventListener("keydown", arrowKeys);
-  });
-
-  //componentWillUnmount
-  useEffect(() => {
+    let tempTime = 5;
+    const myTimer = setInterval(() => {
+      setTimeRemaining(tempTime);
+      if (tempTime > 0) {
+        tempTime = tempTime - 1;
+      } else {
+        clearInterval(myTimer);
+        getNewDog();
+      }
+    }, 1000);
+    //componentWillUnmount
     return () => {
       window.removeEventListener("keydown", arrowKeys);
+      clearInterval(myTimer);
     };
-  });
+  }, [dog]);
 
   return (
     <div className="app">
@@ -43,11 +57,12 @@ function App() {
       </h3>
       <section>
         Current Dog
-        <img src={dog} />
-      </section>
-      <section>
+        <img src={dog} width="300px" />
+        Time Remaining: {timeRemaining}
         Adopted Dogs
-        <div>{adopted}</div>
+        {adopted.map((dogUrl) => {
+          return <img src={dogUrl} width="200px" />;
+        })}
       </section>
     </div>
   );
