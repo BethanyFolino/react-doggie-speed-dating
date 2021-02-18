89ac214c55c96885798181ae53c81540b600ff9d
BETHANY FOLINO <bethany.folino@gmail.com>
2021-02-17 17:00:18 -0800

    Fixed dog photo issue

--- App.js
+++ App.js
@@ -12,42 +12,9 @@ function App() {
   //componentDidMount
   useEffect(() => {
     fetch("https://dog.ceo/api/breeds/image/random")
-      .then((object) => object.json())
-      .then((hydrated) => console.log(hydrated))
-      .then((hydrated) => setDog(hydrated));
+      .then((res) => res.json())
+      .then((data) => setDog(data.message));
   }, []);
-
-  //left arrow key function
-  // const leftToSkip = (e) => {
-  //   //get rid of current dog photo when left arrow is pushed and move on to next dog
-  //   dog.picture += `${e.code}`;
-  //   if (e.code === "ArrowLeft") {
-  //     dog.pop(dog.picture);
-  //   }
-  // };
-
-  //right arrow key function
-  // const rightToAdopt = (e) => {
-  //   //move current dog photo to adopted zone when right arrow is pushed and put a new one on screen
-  //   dog.picture += `${e.code}`;
-  //   if (e.code === "ArrowRight") {
-  //     adopted.push(dog.picture);
-  //   }
-  // };
-
-  //componentDidUpdate
-  // useEffect(() => {
-  //   window.addEventListener("keydown", leftToSkip);
-  //   window.addEventListener("keydown", rightToAdopt);
-  // });
-
-  //componentWillUnmount
-  // useEffect(() => {
-  //   return (
-  //     window.removeEventListener("keydown", leftToSkip),
-  //     window.removeEventListener("keydown", rightToAdopt)
-  //   );
-  // });
 
   return (
     <div className="app">
@@ -55,9 +22,10 @@ function App() {
       <h3>
         Press the arrow keys on your keyboard. Left to skip, Right to Adopt.
       </h3>
-      <div>
-        <img url={dog.object.message} />
-      </div>
+      <section>
+        Current Dog
+        <img src={dog} />
+      </section>
     </div>
   );
 }
